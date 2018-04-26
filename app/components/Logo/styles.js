48ed9df7558333a83926575211c7b6a204f597e8
Ramasubbaiya Adaikkalam <ramasubbaiya@gmail.com>
2018-04-25 21:56:43 -0400

    Logo animation and Keyboard dismiss event added

--- app/components/Logo/styles.js
+++ app/components/Logo/styles.js
@@ -3,18 +3,29 @@ import EStlyeSheet from 'react-native-extended-stylesheet';
 
 const imageWidth = Dimensions.get('window').width / 2;
 
-export default EStlyeSheet.create({
+const styles = EStlyeSheet.create({
+    $largeContainerSize: imageWidth,
+    $largeImageSize: imageWidth / 2,
+    $smallContainerSize: imageWidth / 2,
+    $smallImageSize: imageWidth / 4,
+
     container: {
         alignItems: 'center',
     },
     containerImage: {
         alignItems: 'center',
         justifyContent: 'center',
-        width: imageWidth,
-        height: imageWidth,
+        width: '$largeContainerSize',
+        height: '$largeContainerSize',
+    },
+    backgroundImage: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        alignSelf: 'stretch',
+        flex: 1,
     },
     image: {
-        width: imageWidth / 2,
+        width: '$largeImageSize',
     },
     text: {
         fontWeight: '600',
@@ -24,3 +35,5 @@ export default EStlyeSheet.create({
         color: '$white',
     }
 });
+
+export default styles;
--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,12 +1,77 @@
-import React from 'react';
-import { View, StatusBar } from 'react-native';
+import React, { Component } from 'react';
+import { StatusBar, KeyboardAvoidingView } from 'react-native';
 
 import { Container } from '../components/Container';
 import { Logo } from '../components/Logo';
+import { InputWithButton } from '../components/TextInput';
+import { ClearButton } from '../components/Button';
+import { LastConverted } from '../components/Text';
+import { Header } from '../components/Header';
 
-export default () => (
-  <Container>
-    <StatusBar translucent={false} barStyle="light-content" />
-    <Logo />
-  </Container>
-);
+const TEMP_BASE_CURRENCY = 'USD';
+const TEMP_QUOTE_CURRENCY = 'GBP';
+const TEMP_BASE_PRICE = '100';
+const TEMP_QUOTE_PRICE = '79.74';
+
+const TEMP_CONVERSION_RATE = 0.7974;
+const TEMP_CONVERSION_DATE = new Date();
+
+class Home extends Component {
+  handlePressBaseCurrency = () => {
+    console.log('press base');
+  }
+
+  handlePressQuoteCurrency = () => {
+    console.log('press quote');
+  }
+
+  handleTextChange = (text) => {
+    console.log('change text', text);
+  }
+
+  handleSwapCurrency = () => {
+    console.log('swap');
+  }
+
+  handleOptionsPress = () => {
+    console.log('gear pressed');
+  }
+
+  render() {
+    return (
+      <Container>
+        <StatusBar translucent={false} barStyle="light-content" />
+        <Header onPress={this.handleOptionsPress} />
+        <KeyboardAvoidingView behavior="padding">
+          <Logo />
+          <InputWithButton
+            buttonText={TEMP_BASE_CURRENCY}
+            onPress={this.handlePressBaseCurrency}
+            defaultPrice={TEMP_BASE_PRICE}
+            keyboardType="numeric"
+            value={TEMP_BASE_PRICE}
+            onChangeText={this.handleTextChange}
+          />
+          <InputWithButton
+            buttonText={TEMP_QUOTE_CURRENCY}
+            onPress={this.handlePressQuoteCurrency}
+            editable={false}
+            value={TEMP_QUOTE_PRICE}
+          />
+          <LastConverted
+            base={TEMP_BASE_CURRENCY}
+            quote={TEMP_QUOTE_CURRENCY}
+            conversionRate={TEMP_CONVERSION_RATE}
+            date={TEMP_CONVERSION_DATE}
+          />
+          <ClearButton
+            text="Reverse Currency"
+            onPress={this.handleSwapCurrency}
+          />
+        </KeyboardAvoidingView>
+      </Container>
+    );
+  }
+}
+
+export default Home;
